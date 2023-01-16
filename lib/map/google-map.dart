f5e4a5e77232586159d7439c82940f86afc8ba74
oussama-alouache <oussamaalouacheedv@gmail.com>
2023-01-16 22:44:56 +0100

    geocoding

--- lib/map/google-map.dart
+++ lib/map/google-map.dart
@@ -3,13 +3,14 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:selkni/constant.dart';
 
 class MapG extends StatefulWidget {
   Position? position;
-
+  Position? _position;
   bool isload = false;
   @override
   State<MapG> createState() => _MapGState();
@@ -37,15 +38,37 @@ class _MapGState extends State<MapG> {
 
   // late LatLng currrentlocation =
   //     LatLng(currentlocation!.latitude!, currentlocation!.longitude!);
-  // ignore: prefer_typing_uninitialized_variables
+  // ignore: prefer_typing_uninit
+  // ialized_variables
 
+  void lov() {
+    Geolocator.getPositionStream().listen((Position position) {
+      widget.position = position;
+    });
+  }
+
+// create this variable
+  String _address = "";
   void Getlocalpotion() async {
     LocationPermission permission = await Geolocator.checkPermission();
     widget.position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.best);
+    List<Placemark> newPlace = await placemarkFromCoordinates(
+        widget.position!.latitude, widget.position!.longitude);
+    Placemark placeMark = newPlace[0];
+    String name = placeMark.name.toString();
+    String subLocality = placeMark.subLocality.toString();
+    String locality = placeMark.locality.toString();
+    String aministrativeArea = placeMark.administrativeArea.toString();
+    String postalCode = placeMark.postalCode.toString();
+    String country = placeMark.country.toString();
+    String address =
+        "${name},${locality},${aministrativeArea} ${postalCode}, ${country}";
+    print('${address}');
     print('${widget.position!.longitude}');
 
     setState(() {
+      _address = address;
       widget.isload = true;
     });
   }
@@ -56,7 +79,7 @@ class _MapGState extends State<MapG> {
     PolylinePoints polylinePoints = PolylinePoints();
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
       google_api_key,
-      PointLatLng(mycurrentposition.latitude, mycurrentposition.longitude),
+      PointLatLng(mycurrentposition!.latitude, mycurrentposition!.longitude),
       PointLatLng(destinationposition.latitude, destinationposition.longitude),
     );
     if (result.points.isNotEmpty) {
@@ -64,7 +87,9 @@ class _MapGState extends State<MapG> {
         (PointLatLng point) =>
             Polylinecord.add(LatLng(point.latitude, point.longitude)),
       );
-      setState(() {});
+      setState(() {
+        widget.isload = true;
+      });
     }
   }
 
@@ -72,8 +97,7 @@ class _MapGState extends State<MapG> {
   void initState() {
     // TODO: implement initState
     GetPolyPoint();
-    StreamSubscription<ServiceStatus> serviceStatusStream =
-        Geolocator.getServiceStatusStream().listen((currentposition) {});
+
     Getlocalpotion();
     super.initState();
   }
@@ -81,15 +105,23 @@ class _MapGState extends State<MapG> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            _address,
+            style: TextStyle(fontSize: 17),
+          ),
+        ),
+      ),
       body: widget.isload
           ? GoogleMap(
-              initialCameraPosition: CameraPosition(
+              initialCameraPosition: const CameraPosition(
                 target: mycurrentposition,
                 zoom: 14,
               ),
               polylines: {
                 Polyline(
-                  polylineId: PolylineId("value"),
+                  polylineId: const PolylineId("value"),
                   points: Polylinecord,
                   color: Colors.deepOrange,
                   width: 6,
@@ -97,9 +129,10 @@ class _MapGState extends State<MapG> {
               },
               onMapCreated: (controller) {
                 mapController = controller;
-                addMarker('test', mycurrentposition);
-                addMarker('test1', currentposition);
-                addMarker('test2', destinationposition);
+                addMarker('test', mycurrentposition, mycurrentposition);
+                addMarker('test1', currentposition, currentposition);
+                addMarker('test8', currentposition, currentposition);
+                addMarker('test2', destinationposition, destinationposition);
               },
               markers: _Markers.values.toSet(),
               myLocationEnabled: true,
@@ -109,10 +142,11 @@ class _MapGState extends State<MapG> {
     );
   }
 
-  addMarker(String id, LatLng location) {
+  addMarker(String id, LatLng location, LatLng info) {
     var marker = Marker(
       markerId: MarkerId(id),
       position: location,
+      infoWindow: InfoWindow(),
     );
     _Markers[id] = marker;
     setState(() {});
